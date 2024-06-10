52b81b8f7fb779d3513579508738f93f0be0c871
allxdrstp <allxdrstp@mail.ru>
2024-06-10 22:16:24 +0300

    add all

---
+++ build/CMakeFiles/FactoryPatternExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FactoryPatternExample.dir/src/main.cpp.o"
+  "CMakeFiles/FactoryPatternExample.dir/src/main.cpp.o.d"
+  "FactoryPatternExample"
+  "FactoryPatternExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FactoryPatternExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/FactoryPatternExample.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FactoryPatternExample.
+# This may be replaced when dependencies are built.
